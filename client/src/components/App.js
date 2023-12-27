072674e051ee77c32977b42e31fc49f15987e484
premnarayanp <premnarayanpatel223@gmail.com>
2023-12-27 12:25:54 +0530

    MERN projects structure as client & server

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/app.css'
+class App extends React.Component{
+
+    render(){
+
+    return (
+      <div className="App">
+      My app
+      </div>
+    );
+  }
+
+}
+export default App;
